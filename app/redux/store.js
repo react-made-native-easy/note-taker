ff88941c5f4ece78e805e705512a7ebd40fcb647
Atul <atulanand94@gmail.com>
2017-09-09 16:04:25 +0700

    Fixed devtools extension (#14)
    * Added bottom character count view
    
    * Moved state to parent component
    
    * redux setup complete
    
    * redux setup complete
    
    * fixed new devtools extension

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,18 +1,18 @@
-import {createStore/* ,compose, applyMiddleware*/} from 'redux';
+import {createStore, compose/* , applyMiddleware*/} from 'redux';
 // import someReduxMiddleware from 'some-redux-middleware';
 // import someOtherReduxMiddleware from 'some-other-redux-middleware';
 import rootReducer from './reducers/root.reducer';
 
 const enhancerList = [];
-const devToolsExtension = window && window.devToolsExtension;
+const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
 
 if (typeof devToolsExtension === 'function') {
   enhancerList.push(devToolsExtension());
 }
 
-// const composedEnhancer = compose(applyMiddleware(someReduxMiddleware, someOtherReduxMiddleware), ...enhancerList);
+const composedEnhancer = compose(/* applyMiddleware(someReduxMiddleware, someOtherReduxMiddleware),*/ ...enhancerList);
 
-const initStore = () => createStore(rootReducer, {} /* , composedEnhancer*/);
+const initStore = () => createStore(rootReducer, {}, composedEnhancer);
 
 module.exports = {
   initStore
