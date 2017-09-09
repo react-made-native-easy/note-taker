34133e2c8260b98b6a5f6d874711e407455a5ec2
Atul R <atulanand94@gmail.com>
2017-09-09 16:25:47 +0700

    Added presentational and container components

--- app/App.container.js
+++ app/App.container.js
@@ -1,22 +1,13 @@
 import React, {Component} from 'react';
-import Home from './components/Home/Home.component';
+import Home from './pages/Home.page';
 import {connect} from 'react-redux';
 
 class App extends Component {
   render () {
-    console.log(this.props.state); // eslint-disable-line
     return (
       <Home />
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  state
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  dispatch
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(null, null)(App);
--- app/redux/actions/index.actions.js
+++ app/redux/actions/index.actions.js
@@ -1,1 +1,17 @@
+import {createAction} from 'redux-actions';
+
 export const TEST_ACTION = 'TEST_ACTION';
+export const SET_TEXT = 'SET_TEXT';
+export const SET_TITLE = 'SET_TITLE';
+
+
+export const setTitle = createAction(SET_TITLE);
+/* This is equivalent to
+export const setTitle = (payload) => {
+  return {
+    type: SET_TITLE,
+    payload: payload
+  };
+};
+*/
+export const setText = createAction(SET_TEXT);
