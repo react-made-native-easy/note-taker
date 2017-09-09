c367b16b0245e9c611ffe5e1536fc07d85ee7e1b
Atul <atulanand94@gmail.com>
2017-09-09 16:44:31 +0700

    Added presentational and container components (#15)

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -2,30 +2,34 @@ import React, {Component} from 'react';
 import {View, Text, TextInput} from 'react-native';
 import styles from './Home.component.style';
 import TextArea from '../TextArea/TextArea.component';
+import PropTypes from 'prop-types';
 
 class Home extends Component {
-  state = {
-    title: '',
-    text: ''
-  }
-  setTitle = (title) => this.setState({title})
-  setText = (text) => this.setState({text});
   render () {
+    const {setTitle, title, text, setText} = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.titleHeading}> Note Title</Text>
         <TextInput style={styles.titleTextInput}
-            onChangeText={this.setTitle} value={this.state.title} />
+            onChangeText={setTitle} value={title} />
         <Text style={styles.textAreaTitle}> Please type your note below </Text>
-        <TextArea text={this.state.text} onTextChange={this.setText} style={styles.textArea}/>
+        <TextArea text={text} onTextChange={setText} style={styles.textArea}/>
         <View style={styles.bottomBar}>
           <View style={styles.bottomBarWrapper}>
             <Text style={styles.saveBtn}>Save</Text>
-            <Text style={styles.characterCount}>{this.state.text.length} characters</Text>
+            <Text style={styles.characterCount}>{text.length} characters</Text>
           </View>
         </View>
       </View>
     );
   }
 }
+
+Home.propTypes = {
+  setTitle: PropTypes.func,
+  setText: PropTypes.func,
+  title: PropTypes.string,
+  text: PropTypes.string
+};
+
 export default Home;
