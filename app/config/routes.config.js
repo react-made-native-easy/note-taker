7f7d347184b3ad90424470587516f5ce3219db31
Rahul gaba <rahulgaba16@gmail.com>
2017-09-09 15:25:58 +0530

    File Structure for routes

---
+++ app/config/routes.config.js
@@ -0,0 +1,8 @@
+export const aboutRoutesConfig = {
+  tabBarOptions: {
+    upperCaseLabel: false,
+    showIcon: false
+  },
+  swipeEnabled: true,
+  animationEnabled: true
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,32 +1,10 @@
-import {StackNavigator, TabNavigator} from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
 import HomePage from '../pages/Home.page';
-import AboutApp from '../components/About/AboutApp.component';
-import AboutDevs from '../components/About/AboutDevs.component';
-
-const AboutRoutes = TabNavigator({
-  aboutApp: {
-    screen: AboutApp,
-    navigationOptions: {
-      title: 'About the App'
-    }
-  },
-  aboutDevs: {
-    screen: AboutDevs,
-    navigationOptions: {
-      title: 'About the Creators'
-    }
-  }
-}, {
-  tabBarOptions: {
-    upperCaseLabel: false,
-    showIcon: false
-  },
-  swipeEnabled: true,
-  animationEnabled: true
-});
+import AboutRoutes from './about.routes.js';
 
 const Router = StackNavigator({
-  home: {screen: HomePage,
+  home: {
+    screen: HomePage,
     navigationOptions: {
       title: 'Start taking notes',
     }
@@ -34,8 +12,6 @@ const Router = StackNavigator({
   about: {
     screen: AboutRoutes
   }
-}, {
-  mode: 'card'
 });
 
 export default Router;
