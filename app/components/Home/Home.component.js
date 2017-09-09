f4e967c9db8d33b0b8f0025547b5df885000627b
Atul R <atulanand94@gmail.com>
2017-09-09 12:50:58 +0700

    Moved state to parent component

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -5,26 +5,27 @@ import TextArea from '../TextArea/TextArea.component';
 
 class Home extends Component {
   state = {
-    title: '' // adding the state here temporarily for illustration purposes
+    title: '',
+    text: ''
   }
   setTitle = (title) => this.setState({title})
+  setText = (text) => this.setState({text});
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.titleHeading}> Note Title</Text>
         <TextInput style={styles.titleTextInput}
-          onChangeText={this.setTitle} value={this.state.title} />
+            onChangeText={this.setTitle} value={this.state.title} />
         <Text style={styles.textAreaTitle}> Please type your note below </Text>
-        <TextArea style={styles.textArea}/>
+        <TextArea text={this.state.text} onTextChange={this.setText} style={styles.textArea}/>
         <View style={styles.bottomBar}>
           <View style={styles.bottomBarWrapper}>
             <Text style={styles.saveBtn}>Save</Text>
-            <Text style={styles.characterCount}>{20} characters</Text>
+            <Text style={styles.characterCount}>{this.state.text.length} characters</Text>
           </View>
         </View>
       </View>
     );
   }
 }
-
 export default Home;
