227bad739041becf742ccfc29270c729339f896d
Rahul gaba <rahulgaba16@gmail.com>
2017-09-11 00:53:37 +0530

    navigation text from translation

--- app/routes/about.routes.js
+++ app/routes/about.routes.js
@@ -2,18 +2,19 @@ import {TabNavigator} from 'react-navigation';
 import AboutApp from '../components/About/AboutApp.component';
 import AboutDevs from '../components/About/AboutDevs.component';
 import {aboutRoutesConfig} from '../config/routes.config';
+import translate from '../utils/language.utils';
 
 export default TabNavigator({
   aboutApp: {
     screen: AboutApp,
     navigationOptions: {
-      title: 'About the App'
+      title: translate('ABOUT_theApp')
     }
   },
   aboutDevs: {
     screen: AboutDevs,
     navigationOptions: {
-      title: 'About the Creators'
+      title: translate('ABOUT_theCreators')
     }
   }
 }, aboutRoutesConfig);
