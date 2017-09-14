0ed352f1cfc615a20045c5fe28b8fd81b8bc1797
Rahul gaba <rahulgaba16@gmail.com>
2017-09-15 00:03:10 +0530

    Added notes add feature

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, KeyboardAvoidingView} from 'react-native';
 import styles from './Home.component.style';
-import TextArea from '../TextArea/TextArea.component';
+import Notes from '../Notes/Notes.component.js';
 import PropTypes from 'prop-types';
 
 class Home extends Component {
+  addNote = () => {
+    const {saveNote, title, text} = this.props;
+    saveNote({title, text});
+  }
   render () {
-    const {setTitle, title, text, setText} = this.props;
+    const {setTitle, title, text, setText, notes} = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.titleHeading}> Note Title</Text>
         <TextInput style={styles.titleTextInput}
             onChangeText={setTitle} value={title} />
         <Text style={styles.textAreaTitle}> Please type your note below </Text>
-        <TextArea text={text} onTextChange={setText} style={styles.textArea}/>
-        <View style={styles.bottomBar}>
+        <TextInput style={styles.textArea} multiline = {true}
+          onChangeText={setText} value={text}/>
+        <KeyboardAvoidingView style={styles.bottomBar}>
           <View style={styles.bottomBarWrapper}>
-            <Text style={styles.saveBtn}>Save</Text>
+            <Text style={styles.saveBtn} onPress={this.addNote}>Save</Text>
             <Text style={styles.characterCount}>{text.length} characters</Text>
           </View>
-        </View>
+        </KeyboardAvoidingView>
+        <Notes data={notes} />
       </View>
     );
   }
@@ -27,8 +33,10 @@ class Home extends Component {
 
 Home.propTypes = {
   setTitle: PropTypes.func,
+  saveNote: PropTypes.func,
   setText: PropTypes.func,
   title: PropTypes.string,
+  notes: PropTypes.array,
   text: PropTypes.string
 };
 
--- app/pages/Home.page.js
+++ app/pages/Home.page.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {setTitle, setText} from '../redux/actions/index.actions';
+import {setTitle, setText, addNote} from '../redux/actions/index.actions';
 import Home from '../components/Home/Home.component';
 
 class HomePage extends Component {
   render () {
-    const {setTitle, setText, title, text} = this.props;
+    const {setTitle, setText, title, text, saveNote, notes} = this.props;
     return (
-      <Home setTitle={setTitle} setText={setText} title={title} text={text} />
+      <Home setTitle={setTitle} notes={notes} saveNote={saveNote} setText={setText} title={title} text={text} />
     );
   }
  }
@@ -16,18 +16,26 @@ class HomePage extends Component {
 HomePage.propTypes = {
   setTitle: PropTypes.func,
   setText: PropTypes.func,
+  saveNote: PropTypes.func,
   title: PropTypes.string,
-  text: PropTypes.string
+  text: PropTypes.string,
+  notes: PropTypes.array,
 };
 
 const mapStateToProps = (state) => ({
   title: state.content.title,
-  text: state.content.text
+  text: state.content.text,
+  notes: state.notes
 });
 
 const mapDispatchToProps = (dispatch) => ({
   setTitle: (title) => dispatch(setTitle(title)),
   setText: (text) => dispatch(setText(text)),
+  saveNote: (note) => {
+    dispatch(addNote(note));
+    dispatch(setTitle(''));
+    dispatch(setText(''));
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
