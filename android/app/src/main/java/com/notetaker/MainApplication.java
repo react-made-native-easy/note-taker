8acafe0f4e39730712ae235262d7cc6daf5b0d37
Atul <atulanand94@gmail.com>
2017-09-14 23:25:58 +0700

    Android native module (#26)
    * Added custom android native module
    
    * mocked for tests

--- android/app/src/main/java/com/notetaker/MainApplication.java
+++ android/app/src/main/java/com/notetaker/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.notetaker.device.DevicePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNI18nPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new DevicePackage()
       );
     }
   };
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {initStore} from './redux/store';
 import {Provider} from 'react-redux';
+import {NativeModules} from 'react-native';
 
 import App from './App.container';
 
@@ -8,6 +9,7 @@ const store = initStore();
 
 class NoteTaker extends Component {
   render () {
+    NativeModules.Device.getDeviceName((err, name) => console.log(err, name)); //eslint-disable-line
     return (
       <Provider store={store}>
         <App />
